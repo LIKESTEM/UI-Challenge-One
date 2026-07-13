06453169bb7b2a112f5817006560614b7ac4a6ef
LIKESTEM <thabisotapala@gmail.com>
2026-07-13 10:51:14 +0200

    added sliding and fadding effects

--- src/components/Partners/Partners.tsx
+++ src/components/Partners/Partners.tsx
@@ -1,28 +1,32 @@
 import Slider from "../Slider/Slider";
 import styles from "./Partners.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaypal, faBitcoin } from "@fortawesome/free-brands-svg-icons"; // or wherever this comes from
+
 
 const PARTNERS = [
-  "PayPal",
-  "coinbase",
-  "BINANCE",
-  "Revolut",
-  "EXODUS",
-  "BITFINEX",
-  "Blockchain",
-  "Stripe",
-  "Kraken",
-  "Ledger",
-  "MetaMask",
-  "OpenSea",
-  "Trust Wallet",
+  { name: "PayPal", icon: faPaypal },
+  { name: "coinbase" },
+  { name: "BINANCE" },
+  { name: "Revolut" },
+  { name: "EXODUS" },
+  { name: "BITFINEX" },
+  { name: "Blockchain" },
+  { name: "Stripe" },
+  { name: "Kraken" },
+  { name: "Ledger" },
+  { name: "MetaMask" },
+  { name: "OpenSea" },
+  { name: "Trust Wallet" },
 ];
 
 function Partners() {
   return (
     <div className={styles.partners}>
       <Slider className={styles.row} direction="left" speed={30}>
-        {PARTNERS.map((name) => (
+        {PARTNERS.map(({ name, icon }) => (
           <span className={styles.brand} key={name}>
+            {icon && <FontAwesomeIcon icon={icon} />}
             {name}
           </span>
         ))}
@@ -32,3 +36,6 @@ function Partners() {
 }
 
 export default Partners;
+
+
+
